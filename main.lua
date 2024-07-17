726dd56de0c72e452b8fa7fdcdb87d0a36920a44
Eva Pace <eba.pachi@gmail.com>
2024-07-17 15:17:03 -0300

    pong-2 (font & render paddles/ball)

--- main.lua
+++ main.lua
@@ -10,6 +10,10 @@ WINDOW_HEIGHT = 720
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    local smallFont = love.graphics.newFont('font.ttf', 8)
+
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(GAME_WIDTH, GAME_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -23,16 +27,33 @@ function love.keypressed(key)
     end
 end
 
+PADDLE_WIDTH = 5
+PADDLE_HEIGHT = 20
+
+BALL_SIDE = 4
+
 function love.draw()
     push:apply('start')
+
+    -- reset background to similar color of original pong
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
     love.graphics.printf(
         'Hello Pong!',
         0,
-        GAME_HEIGHT / 2,
+        20,
         GAME_WIDTH,
         'center'
     )
+
+    -- first paddle (left side)
+    love.graphics.rectangle('fill', 5, 30, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+    -- second paddle (right side)
+    love.graphics.rectangle('fill', GAME_WIDTH - 10, GAME_HEIGHT - 50, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+    -- ball
+    love.graphics.rectangle('fill', GAME_WIDTH / 2 - 2, GAME_HEIGHT / 2 - 2, BALL_SIDE, BALL_SIDE)
 
     push:apply('end')
 end

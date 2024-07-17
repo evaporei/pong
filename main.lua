a1410a1459aeb044b1fce8ac764d96897f18b2fc
Eva Pace <eba.pachi@gmail.com>
2024-07-17 16:09:51 -0300

    pong-4 move ball

--- main.lua
+++ main.lua
@@ -12,11 +12,26 @@ local scoreFont = love.graphics.newFont('font.ttf', 32)
 
 PADDLE_SPEED = 200
 
+PADDLE_WIDTH = 5
+PADDLE_HEIGHT = 20
+
+BALL_SIDE = 4
+
 local player1Score = 0
 local player2Score = 0
 
 local player1Y = 30
 local player2Y = GAME_HEIGHT - 50
+
+-- position
+local ballX = GAME_WIDTH / 2 - (BALL_SIDE / 2)
+local ballY = GAME_HEIGHT / 2 - (BALL_SIDE / 2)
+
+-- velocity
+local ballVX = math.random(2) == 1 and 100 or -100
+local ballVY = math.random(-50, 50)
+
+local gameState = 'start'
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -34,28 +49,40 @@ function love.update(dt)
     -- sorry I use workman layout
     -- this is WASD
     if love.keyboard.isDown('d') then
-        player1Y = player1Y + -PADDLE_SPEED * dt
+        player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)
     elseif love.keyboard.isDown('s') then
-        player1Y = player1Y + PADDLE_SPEED * dt
+        player1Y = math.min(GAME_HEIGHT - PADDLE_HEIGHT, player1Y + PADDLE_SPEED * dt)
     end
 
     if love.keyboard.isDown('up') then
-        player2Y = player2Y + -PADDLE_SPEED * dt
+        player2Y = math.max(0, player2Y + -PADDLE_SPEED * dt)
     elseif love.keyboard.isDown('down') then
-        player2Y = player2Y + PADDLE_SPEED * dt
+        player2Y = math.min(GAME_HEIGHT - PADDLE_HEIGHT, player2Y + PADDLE_SPEED * dt)
+    end
+
+    if gameState == 'play' then
+        ballX = ballX + ballVX * dt
+        ballY = ballY + ballVY * dt
     end
 end
 
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        else
+            gameState = 'start'
+
+            ballX = GAME_WIDTH / 2 - (BALL_SIDE / 2)
+            ballY = GAME_HEIGHT / 2 - (BALL_SIDE / 2)
+
+            ballVX = math.random(2) == 1 and 100 or -100
+            ballVY = math.random(-50, 50)
+        end
     end
 end
-
-PADDLE_WIDTH = 5
-PADDLE_HEIGHT = 20
-
-BALL_SIDE = 4
 
 function love.draw()
     push:apply('start')
@@ -93,7 +120,7 @@ function love.draw()
     love.graphics.rectangle('fill', GAME_WIDTH - 10, player2Y, PADDLE_WIDTH, PADDLE_HEIGHT)
 
     -- ball
-    love.graphics.rectangle('fill', GAME_WIDTH / 2 - 2, GAME_HEIGHT / 2 - 2, BALL_SIDE, BALL_SIDE)
+    love.graphics.rectangle('fill', ballX, ballY, BALL_SIDE, BALL_SIDE)
 
     push:apply('end')
 end
